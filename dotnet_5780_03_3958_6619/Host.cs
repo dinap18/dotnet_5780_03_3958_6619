698e5c4e962fc3ed2853683fa5df7f7e09fc0904
Dina Pinchuck <57219508+dinap18@users.noreply.github.com>
2019-12-01 16:25:44 +0200

    Add files via upload

---
+++ dotnet_5780_03_3958_6619/Host.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_5780_03_3958_6619
+{
+   public class Host
+    {
+        string HostName;
+        List<HostingUnit> Units;
+    }
+}
---
+++ dotnet_5780_03_3958_6619/HostingUnit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_5780_03_3958_6619
+{
+    public class HostingUnit
+    {
+        string UnitName;
+        int rooms;
+        bool IsSwimmingPool;
+        public List <DateTime>AllOrders { get; set; }
+        public List<string>Uris { get; set; }
+    }
+}
